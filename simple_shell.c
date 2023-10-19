361fcb56d09c8b239e92d6ac3ecc6f1b5de1f80e
adejoh-godwin <godwinadejoh@gmail.com>
2023-10-19 16:22:35 -0700

    betty check

--- simple_shell.c
+++ simple_shell.c
@@ -1,58 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
-#define MAX_COMMAND_LENGTH 100
+#define MAX_INPUT_SIZE 1024
 
-int main(void)
-{
-    char *command = NULL;
-    size_t command_length = 0;
-    char *args[2];
-    char *envp[] = { NULL };
+void display_prompt() {
+    printf("$ ");
+}
 
-    while (1)
-    {
-        /* Display prompt and wait for user input */
-        write(1, "$ ", 2);
-        fsync(1);
+int main() {
+    char input[MAX_INPUT_SIZE];
+    char* args[2];
 
-        /* Read user input */
-        if (getline(&command, &command_length, stdin) == -1)
-        {
-            free(command);
-            write(1, "\n", 1);
-            break; /* Handle end of file (Ctrl+D) */
+    while (1) {
+        display_prompt();
+
+        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+            printf("\n");
+            break;  // Handle Ctrl+D (end of file)
         }
 
-        /* Remove trailing newline character */
-        command[strcspn(command, "\n")] = 0;
+        // Remove the trailing newline character
+        input[strcspn(input, "\n")] = '\0';
 
-        /* Check if executable exists */
-        if (access(command, X_OK) != 0)
-        {
-            write(2, "Error: Command not found\n", 25);
-            continue;
-        }
+        pid_t pid = fork();
 
-        /* Execute command */
-        args[0] = command;
-        args[1] = NULL;
-        if (fork() == 0)
-        {
-            execve(command, args, envp);
-            perror("execve");
+        if (pid == -1) {
+            perror("fork");
             exit(EXIT_FAILURE);
-        }
-        else
-        {
-            wait(NULL); /* Wait for child process to finish */
+        } else if (pid == 0) {
+            // Child process
+            args[0] = input;
+            args[1] = NULL;
+
+            if (execve(args[0], args, NULL) == -1) {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // Parent process
+            int status;
+            if (wait(&status) == -1) {
+                perror("wait");
+                exit(EXIT_FAILURE);
+            }
         }
     }
 
-    free(command);
     return 0;
 }
